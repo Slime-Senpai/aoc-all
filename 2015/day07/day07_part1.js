dea07d15dbd424dc0701fc27b55732ac8fbdfa43
Slime-Senpai <mrthetawtute@hotmail.fr>
2023-12-28 01:15:12 +0100

    day07 start

--- 2015/day07/day07_part1.js
+++ 2015/day07/day07_part1.js
@@ -1,3 +1,7 @@
 const fs = require('fs');
 
 const inputText = fs.readFileSync('./day07_example.txt', { encoding: 'utf-8' });
+
+const wires = inputText.split('\n').map((e) => {
+	const [instruction, exitNode] = e.split(' -> ');
+});
