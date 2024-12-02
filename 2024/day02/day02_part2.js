5114f3c6dbcbe4949f14653479069966e085b12c
Slime-Senpai <mrthetawtute@hotmail.fr>
2024-12-02 20:35:47 +0100

    2024 day02 part2

--- 2024/day02/day02_part2.js
+++ 2024/day02/day02_part2.js
@@ -4,40 +4,46 @@ const inputText = fs.readFileSync('./day02_input.txt', { encoding: 'utf-8' });
 
 const reports = inputText.split('\n').map(e => e.split(/\s+/).map(level => +level));
 
-const safeReports = reports.filter(report => {
-    const isDown = report[0] > report[1];
+// Being smart is for smart people, I am stupid therefore I'll check EVERY COMBINATION
+const allReportsPossibilities = reports.map(report => {
+    const combination = [[...report]];
 
-    let lastLevel = report[0];
-
-    let hasDampened = false;
-
-    for (const level of report.slice(1)) {
-        if ((isDown && level > lastLevel) || (!isDown && level < lastLevel)) {
-            if (!hasDampened) {
-                hasDampened = true;
-
-                continue;
-            }
-
-            return false;
-        }
-
-        const diff = Math.abs(level - lastLevel);
-
-        if (diff < 1 || diff > 3) {
-            if (!hasDampened) {
-                hasDampened = true;
-
-                continue;
-            }
-
-            return false;
-        }
-
-        lastLevel = level;
+    for (let i = 0; i < report.length; i++) {
+        combination.push([...report.slice(0, i), ...report.slice(i + 1)]);
     }
 
-    return true;
+    return combination;
 });
+
+const safeReports = allReportsPossibilities.filter(reportCombination => {
+    for (const report of reportCombination) {
+        const isDown = report[0] > report[1];
+
+        let lastLevel = report[0];
+
+        let isOk = true;
+
+        for (const level of report.slice(1)) {
+            if ((isDown && level > lastLevel) || (!isDown && level < lastLevel)) {
+                isOk = false;
+                break;
+            }
+
+            const diff = Math.abs(level - lastLevel);
+
+            if (diff < 1 || diff > 3) {
+                isOk = false;
+                break;
+            }
+
+            lastLevel = level;
+        }
+
+        if (isOk) {
+            return true;
+        }
+
+    }
+})
 
 console.log(safeReports.length);
