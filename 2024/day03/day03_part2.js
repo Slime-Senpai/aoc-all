abf547d7a0ee6b0d44a87745b00712796178052d
Slime-Senpai <mrthetawtute@hotmail.fr>
2024-12-03 10:04:01 +0100

    2024 day03 part1 and part2

--- 2024/day03/day03_part2.js
+++ 2024/day03/day03_part2.js
@@ -2,13 +2,27 @@ const fs = require('fs');
 
 const inputText = fs.readFileSync('./day03_input.txt', { encoding: 'utf-8' });
 
-const mulRegex = /mul\((\d+),(\d+)\)/g;
+const mulRegex = /mul\((\d+),(\d+)\)|do\(\)|don\'t\(\)/g;
 
 const matches = inputText.matchAll(mulRegex);
 
 
 let total = 0;
+
+let enabled = true;
 for (const match of matches) {
+	if (match[0] === 'do()') {
+		enabled = true;
+		continue;
+	} else if (match[0] === 'don\'t()') {
+		enabled = false;
+		continue;
+	}
+
+	if (!enabled) {
+		continue;
+	}
+
 	const firstDigit = +match[1];
 	const secondDigit = +match[2];
 
