96f33de195cec96603c8e1a16428abe648cc43d8
Slime-Senpai <mrthetawtute@hotmail.fr>
2024-12-05 10:53:00 +0100

    day05 part1 and part2

--- 2024/day05/day05_part2.js
+++ 2024/day05/day05_part2.js
@@ -16,8 +16,8 @@ rulesStr.forEach((ruleStr) => {
 });
 const manuals = manualsStr.map((manualStr) => manualStr.split(','));
 
-const validManuals = manuals.filter((manual) => {
-	return manual.every((page, i, arr) => {
+const stinkyManuals = manuals.filter((manual) => {
+	return !manual.every((page, i, arr) => {
 		return arr.slice(0, i).every((pageBefore) => {
 			if (!rules.has(page)) {
 				return true;
@@ -28,8 +28,23 @@ const validManuals = manuals.filter((manual) => {
 	});
 });
 
+stinkyManuals.forEach((manual) => {
+	return manual.sort((a, b) => {
+		const aHasBAfter = rules.has(a) && rules.get(a).includes(b);
+		const bHasAAfter = rules.has(b) && rules.get(b).includes(a);
+
+		if (aHasBAfter) {
+			return -1;
+		} else if (bHasAAfter) {
+			return 1;
+		} else {
+			return 0;
+		}
+	});
+});
+
 console.log(
-	validManuals.reduce((a, b) => {
+	stinkyManuals.reduce((a, b) => {
 		return a + +b[Math.ceil(b.length / 2) - 1];
 	}, 0)
 );
