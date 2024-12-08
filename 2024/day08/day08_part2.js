61ab1e90f35697d8d14fdcaac6958ab1dace8475
Slime-Senpai <mrthetawtute@hotmail.fr>
2024-12-08 19:42:26 +0100

    2024 day08

--- 2024/day08/day08_part2.js
+++ 2024/day08/day08_part2.js
@@ -28,18 +28,25 @@ for (let i = 0; i < map.length; i++) {
 	}
 }
 
-function getAntinode(antenna1, antenna2, maxX = map.length, maxY = map.length) {
-	const antinodesX = antenna1.x + antenna1.x - antenna2.x;
-	const antinodesY = antenna1.y + antenna1.y - antenna2.y;
+function getAntinodes(antenna1, antenna2, maxX = map.length, maxY = map.length) {
+	const diffX = antenna1.x - antenna2.x;
+	const diffY = antenna1.y - antenna2.y;
 
-	if (antinodesX >= 0 && antinodesY >= 0 && antinodesX < maxX && antinodesY < maxY) {
-		return {
-			y: antinodesY,
-			x: antinodesX
+	let currPos = {
+		y: antenna1.y,
+		x: antenna1.x
+	};
+
+	const antinodes = [];
+
+	while (currPos.x >= 0 && currPos.y >= 0 && currPos.x < maxX && currPos.y < maxY) {
+		antinodes.push({ ...currPos });
+		currPos = {
+			y: currPos.y + diffY,
+			x: currPos.x + diffX
 		};
 	}
-
-	return null;
+	return antinodes;
 }
 
 const antinodes = new Set();
@@ -49,7 +56,7 @@ for (const [char, antennasForFrequency] of antennas.entries()) {
 		const antennasToCheck = arr.slice(i + 1);
 
 		for (const antennaToCheck of antennasToCheck) {
-			const antinodesOfAntennas = [getAntinode(antenna, antennaToCheck), getAntinode(antennaToCheck, antenna)].filter((e) => e);
+			const antinodesOfAntennas = [...getAntinodes(antenna, antennaToCheck), ...getAntinodes(antennaToCheck, antenna)];
 
 			antinodesOfAntennas.forEach((e) => antinodes.add(e.y + '-' + e.x));
 		}
